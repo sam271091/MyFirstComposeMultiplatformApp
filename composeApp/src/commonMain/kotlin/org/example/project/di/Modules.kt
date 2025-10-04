683e527d66759c4d1e11e6fda2325a2616e42513
sam271091 <sam271091@gmail.com>
2025-10-04 21:29:49 +0400

    Added core directory

--- composeApp/src/commonMain/kotlin/org/example/project/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/di/Modules.kt
@@ -1,6 +1,7 @@
 package org.example.project.di
 
 import org.example.project.Screens.BookList.BookListViewModel
+import org.example.project.core.data.HttpClientFactory
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
@@ -11,7 +12,7 @@ import org.koin.dsl.module
 expect val platformModule: Module
 
 val sharedModule = module {
-//    single { HttpClientFactory.create(get()) }
+    single { HttpClientFactory.create(get()) }
 //    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
 //    singleOf(::DefaultBookRepository).bind<BookRepository>()
 
